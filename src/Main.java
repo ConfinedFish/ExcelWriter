436996b8a967d51f6477a3e975dd0716431d3dc7
Desktop\Alex <zeiglera@cwu.edu>
2018-11-17 17:30:30 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        XSSFWorkbook work = new XSSFWorkbook();
+        XSSFSheet sheet = work.createSheet("Java Book");
+        Object[][] bookData = {
+                {"Head First Java", "Kathy Serria", 79},
+                {"Effective Java", "Joshua Bloch", 36},
+                {"Clean Code", "Robert martin", 42},
+                {"Thinking in Java", "Bruce Eckel", 35},
+        };
+
+        int rowCount = 0;
+
+        for (Object[] aBook : bookData) {
+            Row row = sheet.createRow(++rowCount);
+
+            int columnCount = 0;
+
+            for (Object field : aBook) {
+                Cell cell = row.createCell(++columnCount);
+                if (field instanceof String) {
+                    cell.setCellValue((String) field);
+                } else if (field instanceof Integer) {
+                    cell.setCellValue((Integer) field);
+                }
+            }
+
+        }
+
+
+        try (FileOutputStream outputStream = new FileOutputStream("JavaBooks.xlsx")) {
+            work.write(outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
